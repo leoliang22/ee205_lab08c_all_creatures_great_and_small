f69aaa79e82fd1734ccae982bd6f39cf0110478e
Leo Liang <leoliang@hawaii.edu>
2022-03-08 17:40:31 -1000

    Initial Commit. Added member variables.

--- Animal.cpp
+++ Animal.cpp
@@ -2,16 +2,15 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
 ///
-/// @file main.cpp
+/// @file Animal.cpp
 /// @version 1.0
 ///
 /// @author Leo Liang <leoliang@hawaii.edu>
 /// @date   08_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <iostream>
+#include "Animal.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using namespace std;
+
+const string Animal::kingdom = "Animalia";
